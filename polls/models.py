5c12aeda8824e55b8874150af4f55ba85ded10c8
luyang93 <510426762@qq.com>
2020-04-26 13:44:26 +0800

    Removing hardcoded URLs in templates

--- polls/models.py
+++ polls/models.py
@@ -21,4 +21,4 @@ class Choice(models.Model):
         return self.choice_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.question.pub_date >= timezone.now() - datetime.timedelta(days=1)
