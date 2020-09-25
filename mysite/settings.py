233ee11752897f496ff725d6924b66d7518e7f22
luyang93 <luyang@singleronbio.com>
2020-09-25 17:56:08 +0800

    Design templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,6 +111,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+STATIC_URL = "/static/"
